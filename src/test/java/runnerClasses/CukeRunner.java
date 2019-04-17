27d61754118c48c3e52ae06e3cb5bf81ce73d4f0
shivamVanderlande <48543342+shivamVanderlande@users.noreply.github.com>
2019-04-17 15:01:48 +0530

    Added Rest api calls

---
+++ src/test/java/runnerClasses/CukeRunner.java
@@ -0,0 +1,38 @@
+package runnerClasses;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html" },
+// plugin = {"pretty"},
+//    plugin = {
+//        "html:target/cucumberHtmlReport",
+//        "json:target/cucumber-report.json"
+//    }, // Plugin to generate HTML report and json report
+		features = { "src/test/resources/Features" }, glue = { "gluecode" }, tags = { "@API_Test" })
+public class CukeRunner {
+	@AfterClass
+	public static void setup() {
+		Reporter.loadXMLConfig(new File("src/test/resources/extent-config.xml"));
+		// Reporter.setSystemInfo("Test User", System.getProperty("user.name"));
+		Reporter.setSystemInfo("User Name", "AJ");
+		Reporter.setSystemInfo("Application Name", "Test App ");
+		Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
+		Reporter.setSystemInfo("Environment", "Production");
+		Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+	}
+	
+	@BeforeClass
+	public void launchBrowser() {
+		
+	}
+}
